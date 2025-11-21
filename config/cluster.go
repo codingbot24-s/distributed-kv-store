ce007a8112da24226c7d45bdb2f472341523e081
pcodefromOarch <primecoder8@gmail.com>
2025-11-21 15:33:49 +0530

    add load config

--- config/cluster.go
+++ config/cluster.go
@@ -1,5 +1,11 @@
 package config
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
 type Peer struct {
 	NodeId string `json:"node_id"`
 	Addr   string `json:"addr"`
@@ -9,4 +15,21 @@ type Cluster struct {
 	NodeId string `json:"node_id"`
 	Addr   string `json:"addr"`
 	Peers  []Peer `json:"peers"`
+}
+
+func LoadConfig(path string) (*Cluster, error) {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("read config file error: %s", err.Error())
+	}
+	var c Cluster
+	if err := json.Unmarshal(content, &c); err != nil {
+		return nil, fmt.Errorf("unmarshal config file error: %s", err.Error())
+	}
+
+	return &c, nil
+}
+
+func (c *Cluster) CreatePeer() {
+	
 }
